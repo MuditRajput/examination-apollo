0dc2d072e9beaeae1b0ce2b280a40d71cd8f6061
Mudit Rajput <mudit.rajput@successive.tech>
2021-03-15 18:33:05 +0530

    feat: add number of attempts

--- src/modules/examination/mutations.js
+++ src/modules/examination/mutations.js
@@ -1,21 +1,21 @@
 export default {
-    createExamination: async (parent, args, context) => {
+  createExamination: async (parent, args, context) => {
+    const { dataSources: { examinationApi } = {} } = context;
+    const { subject, description, maximumMarks, time, maxAttempts } = args;
+    const response = await examinationApi.create({ subject, description, maximumMarks, time, maxAttempts });
+    return response;
+  },
+  updateExamination: async (parent, args, context) => {
       const { dataSources: { examinationApi } = {} } = context;
-      const { subject, description, maximumMarks, time } = args;
-      const response = await examinationApi.create({ subject, description, maximumMarks, time });
+      const { payload: { originalId, dataToUpdate } } = args;
+      const response = await examinationApi.update({ originalId, dataToUpdate });
       return response;
-    },
-    updateExamination: async (parent, args, context) => {
-        const { dataSources: { examinationApi } = {} } = context;
-        const { payload: { originalId, dataToUpdate } } = args;
-        const response = await examinationApi.update({ originalId, dataToUpdate });
-        return response;
-    },
-    deleteExamination: async (parent, args, context) => {
-        const { id = '' } = args;
-        const { dataSources: { examinationApi, questionApi } = {} } = context;
-        const response = await examinationApi.deleteExam(id);
-        questionApi.deleteAllQuestions(id)
-        return response;
-    },
+  },
+  deleteExamination: async (parent, args, context) => {
+      const { id = '' } = args;
+      const { dataSources: { examinationApi, questionApi } = {} } = context;
+      const response = await examinationApi.deleteExam(id);
+      questionApi.deleteAllQuestions(id)
+      return response;
+  },
 }
